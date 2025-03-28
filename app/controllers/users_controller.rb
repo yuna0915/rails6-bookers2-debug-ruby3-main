45a3870d937d2bb980397c9a8e51b30821cb5950
EC2 Default User <ec2-user@ip-172-31-41-246.ap-northeast-1.compute.internal>
2025-03-28 16:43:32 +0000

    [Start] booker2-RSpec

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,15 +10,18 @@ class UsersController < ApplicationController
   def index
     @users = User.all
     @book = Book.new
+    @books = Book.all
+  end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
     if @user.update(user_params)
       redirect_to users_path(@user), notice: "You have updated user successfully."
     else
-      render "show"
+      render "edit"
     end
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  belongs_to :books
+  has_many :books
   has_one_attached :profile_image
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
-
   
   
   def get_profile_image
