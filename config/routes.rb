997a388abaf0b533ec31eb2f0f4019fd31ed3f2b
EC2 Default User <ec2-user@ip-172-31-41-246.ap-northeast-1.compute.internal>
2025-04-02 09:18:25 +0000

    [Finish] favorite

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,8 @@ Rails.application.routes.draw do
 
   get "home/about" , to: "homes#about" , as: "about" 
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
-    resources :book_comments, only: [:create]
     resource :favorite, only: [:create, :destroy]
+    resources :book_comments, only: [:create]
   end
   resources :users, only: [:index,:show,:edit,:update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2025_04_02_060631) do
+ActiveRecord::Schema.define(version: 2025_04_02_081649) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -58,7 +58,7 @@ ActiveRecord::Schema.define(version: 2025_04_02_060631) do
 
   create_table "favorites", force: :cascade do |t|
     t.integer "user_id"
-    t.integer "post_image_id"
+    t.integer "book_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
