6bd1e3075c46d643b3bc35c4a75ac4ca59490ceb
EC2 Default User <ec2-user@ip-172-31-41-246.ap-northeast-1.compute.internal>
2025-04-05 05:35:24 +0000

    [Stumped] search

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,16 +7,18 @@ class User < ApplicationRecord
   has_many :books
   has_many :book_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
-  has_one_attached :profile_image
-
-  # フォローをした、されたの関係
-has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
-has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
   
-# 一覧画面で使う
-has_many :followings, through: :relationships, source: :followed
-has_many :followers, through: :reverse_of_relationships, source: :follower
-
+  # 自分がフォローされる（被フォロー）側の関係性
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  # 被フォロー関係を通じて参照→自分をフォローしている人
+  has_many :followers, through: :reverse_of_relationships, source: :follower
+  
+  # 自分がフォローする（与フォロー）側の関係性
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  # 与フォロー関係を通じて参照→自分がフォローしている人
+  has_many :followings, through: :relationships, source: :followed
+  
+  has_one_attached :profile_image
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
   validates :introduction, length: { maximum: 50 }
@@ -25,20 +27,30 @@ has_many :followers, through: :reverse_of_relationships, source: :follower
   def get_profile_image
     (profile_image.attached?) ? profile_image : 'no_image.jpg'
   end
-
-  # app/models/user.rb
-
-  # フォローしたときの処理
-def follow(user_id)
-  relationships.create!(followed_id: user_id)
-end
-# フォローを外すときの処理
-def unfollow(user_id)
-  relationships.find_by(followed_id: user_id).destroy
-end
-# フォローしているか判定
-def following?(user)
-  followings.include?(user)
-end
   
+  def follow(user)
+    relationships.create(followed_id: user.id)
+  end
+
+  def unfollow(user)
+    relationships.find_by(followed_id: user.id).destroy
+  end
+
+  def following?(user)
+    followings.include?(user)
+  end
+
+  def self.looks(search, word)
+    if search == "perfect_match"
+      @user = User.where("name LIKE?", "#{word}")
+    elsif search == "forward_match"
+      @user = User.where("name LIKE?","#{word}%")
+    elsif search == "backward_match"
+      @user = User.where("name LIKE?","%#{word}")
+    elsif search == "partial_match"
+      @user = User.where("name LIKE?","%#{word}%")
+    else
+      @user = User.all
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,19 @@
 Rails.application.routes.draw do
   devise_for :users
-  
-  root to: "homes#top"
-  get "home/about" => "homes#about", as: "about"
-  
-  resources :books, only: [:index, :show, :edit, :create, :destroy, :update] do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root :to =>"homes#top"
+  get "search" => "searches#search"
+  get "home/about"=>"homes#about", as: 'about'
+
+  resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
     resources :book_comments, only: [:create, :destroy]
     resource :favorites, only: [:create, :destroy]
   end
-
-  resources :users, only: [:index, :show, :edit, :update] do
-    get 'followings', on: :member, to: 'relationships#followings', as: 'followings'
-    get 'followers', on: :member, to: 'relationships#followers', as: 'followers'
-    resource :relationships, only: [:create, :destroy]  
+  resources :users, only: [:index,:show,:edit,:update] do
+    resource :relationships, only: [:create, :destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers'
   end
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
