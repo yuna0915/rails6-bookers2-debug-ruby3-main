b5db6b2084870926a1d9d67f8eac4cd049610f45
EC2 Default User <ec2-user@ip-172-31-41-246.ap-northeast-1.compute.internal>
2025-04-08 13:12:03 +0000

    [Finish] comment-js

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,15 +1,14 @@
 class BookCommentsController < ApplicationController
   def create
     book = Book.find(params[:book_id])
-    comment = current_user.book_comments.new(book_comment_params)
-    comment.book_id = book.id
-    comment.save
-    redirect_to request.referer
+    @comment = current_user.book_comments.new(book_comment_params)
+    @comment.book_id = book.id
+    @comment.save
   end
 
   def destroy
-    BookComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
-    redirect_to request.referer
+    @comment = BookComment.find(params[:id])
+    @comment.destroy
   end
 
   private
@@ -17,3 +16,4 @@ class BookCommentsController < ApplicationController
     params.require(:book_comment).permit(:comment)
   end
 end
+
--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,21 +1,15 @@
 class FavoritesController < ApplicationController
   def create
-    @book = Book.find(params[:book_id])
-    @favorite = current_user.favorites.new(book_id: @book.id)
-    if @favorite.save
-      respond_to do |format|
-        format.js   # これで create.js.erb をレンダリング
-      end
-    end
+    book = Book.find(params[:book_id])
+    @favorite = current_user.favorites.new(book_id: book.id)
+    @favorite.save
+    render 'replace_btn'
   end
 
   def destroy
-    @book = Book.find(params[:book_id])
-    @favorite = current_user.favorites.find_by(book_id: @book.id)
-    if @favorite.destroy
-      respond_to do |format|
-        format.js   # これで create.js.erb をレンダリング
-      end
-    end
+    book = Book.find(params[:book_id])
+    @favorite = current_user.favorites.find_by(book_id: book.id)
+    @favorite.destroy
+    render 'replace_btn'
   end
 end
